24c4235ff74021586a0f29c59eabe6c08dae1575
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-25 09:17:54 -0300

    fix: Models created

---
+++ src/models/Company.js
@@ -0,0 +1,48 @@
+const Company = (sequelize, DataTypes) => {
+  const Company = sequelize.define(
+    "Company",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      totalTraveledWeek: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "total_traveled_week",
+        defaultValue: Math.floor(Math.random() * 10000),
+      },
+      totalOrder: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "total_order",
+        defaultValue: Math.floor(Math.random() * 100),
+      },
+      completedOrders: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "completed_orders",
+        defaultValue: Math.floor(Math.random() * 50),
+      },
+    },
+    {
+      timestamps: false,
+    },
+  );
+
+  return Company;
+};
---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      totalOrder: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "total_order",
+        defaultValue: Math.floor(Math.random() * 1000),
+      },
+      completedOrders: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "completed_orders",
+        defaultValue: Math.floor(Math.random() * 500),
+      },
+    },
+    {
+      timestamps: false,
+    },
+  );
+
+  return User;
+};
