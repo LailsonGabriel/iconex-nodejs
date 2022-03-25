421cbed6d5c251dc470dafc0e5d1df26a7c82887
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-25 09:30:59 -0300

    feat: controller user

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,42 @@
+import rescue from "express-rescue";
+import {
+  getUserById,
+  createUser as create,
+  updateUser as update,
+  deleteUser as userDelete,
+} from "../services/User.services";
+
+const getById = rescue(async (req, res) => {
+  const { id } = req.params;
+
+  const user = await getUserById(id);
+
+  res.status(200).json(user);
+});
+
+const updateUser = rescue(async (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+
+  const userUpdated = await update(body, id);
+
+  res.status(201).json(userUpdated);
+});
+
+const createUser = rescue(async (req, res) => {
+  const user = req.body;
+
+  const userCreated = await create(user);
+
+  res.status(201).json(userCreated);
+});
+
+const deleteUser = rescue(async (req, res) => {
+  const { id } = req.params;
+
+  await userDelete(id);
+
+  res.status(201).json({ message: "Usuário Deletado!" });
+});
+
+export { getById, createUser, updateUser, deleteUser };
