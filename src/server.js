df1a8d3688897bdd50733d37c3bc518af203bad0
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-25 10:53:54 -0300

    fix: added cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+const cors = require("cors");
 
 const CompanyRoutes = require("./routes/company.routes");
 const AuthRoutes = require("./routes/auth.routes");
 const UserRoutes = require("./routes/user.routes");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/auth", AuthRoutes);
 app.use("/company", CompanyRoutes);
