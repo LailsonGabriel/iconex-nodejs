c3ce21496584568084ecd642c3d1f7736a93c94e
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-25 12:04:21 -0300

    fix: login route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,20 @@
 const rescue = require("express-rescue");
 const jwt = require("../utils/jwt");
+const loginService = require("../services/Login.services");
 
 const login = rescue(async (req, res) => {
   const { email } = req.body;
 
   const token = jwt.sign(email);
-  res.status(200).json({ token });
+
+  if (req.route.path === "/user/login") {
+    const user = loginService.getUserByEmail(email);
+    return res.status(200).json({ token, result: user });
+  }
+
+  const company = loginService.getCompanyByEmail(email);
+
+  res.status(200).json({ token, result: company });
 });
 
 module.exports = login;
